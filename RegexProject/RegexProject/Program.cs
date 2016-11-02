c9dc3d8e73e00b49b5a6f63480eeebfe865757de
Anton94 <antonvdudov@gmail.com>
2016-11-02 23:18:53 +0200

    Added some new tests TODO fix new line matching in text field of the leaf element

--- RegexProject/RegexProject/Program.cs
+++ RegexProject/RegexProject/Program.cs
@@ -19,30 +19,28 @@ namespace RegexProject
     public class Program
     {
         // Prints all leafs from given XML file(as string).Each printed leaf has attribute @attr 
-        //and value to this @attr attribute- @attrValue, also the "text" field of the leaf element has @attrValue as word.
+        // and value to this @attr attribute- @attrValue, also the "text" field of the leaf element has @attrValue as word.
         public static void solution(string text, string attr, string attrValue)
         {
+            RegexOptions options = RegexOptions.Multiline; // To match text of leaf with new lines.
+
             // \s*(\w+)\b                               -> Here matches the leaf name
             // .*\s                                     -> some filler and finishes with \s
             // attr + @"\s*=\s*""" + attrValue + @"""   -> here matches the attr = "value" (with as many spaces as it wants)
             // .*>                                      -> some fillers (other attributes or so)
             // (.*\b                                    -> fillers and beginning of a word
-            // + attrValue + @"\b                       -> the attribute value as seperate word in the "text" field of the leaf element
+            // + attrValue + @"\b                       -> the attribute value as separate word in the "text" field of the leaf element
             string regexPattern = @"\s*(\w+)\b.*\s" + attr + @"\s*=\s*""" + attrValue + @""".*>(.*\b" + attrValue + @"\b.*)<";
-          //  string regexPattern = @"<\s*(\w+)[\s\b].*>";
 
-            foreach (Match m in Regex.Matches(text, regexPattern))
+            foreach (Match m in Regex.Matches(text, regexPattern, options))
             {
                 Console.WriteLine("Leaf [{0}] has text [{1}]", m.Groups[1].Value, m.Groups[2].Value); // Prints the leaf name and the 'text' field of element
-              //  Console.WriteLine("{0} {1}", m.Groups[1].Value, m.Groups[2].Value);
             }
         }
 
         public static void Main(string[] args)
         {
             string text = System.IO.File.ReadAllText(@"../../../test.xml");
-
-            //Console.WriteLine(text);
 
             solution(text, "foo", "flas");
         }
