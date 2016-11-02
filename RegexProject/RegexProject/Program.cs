57dbee56eb5d730a377f52e9ea12a84f2b30e6c8
Anton94 <antonvdudov@gmail.com>
2016-11-02 21:34:22 +0200

    Started the first project with regexes

---
+++ RegexProject/RegexProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * 
+ * Name: Anton Dudov
+ * FN:   71488
+ * Task 15 from "Course task 1.pdf"
+ * 
+ * 
+*/
+
+namespace RegexProject
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
