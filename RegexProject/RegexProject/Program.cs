bff2100eecc83ed151d58f63b7c23555ae9339fa
Anton94 <antonvdudov@gmail.com>
2016-11-03 17:00:06 +0200

    Fixed to work with single-line mode of the string

--- RegexProject/RegexProject/Program.cs
+++ RegexProject/RegexProject/Program.cs
@@ -22,16 +22,18 @@ namespace RegexProject
         // and value to this @attr attribute- @attrValue, also the "text" field of the leaf element has @attrValue as word.
         public static void solution(string text, string attr, string attrValue)
         {
-            RegexOptions options = RegexOptions.Multiline; // To match text of leaf with new lines.
+            RegexOptions options = RegexOptions.Singleline;
 
             // \s*(\w+)\b                               -> Here matches the leaf name
-            // .*\s                                     -> some filler and finishes with \s
+            // [^>]*\s                                  -> some filler and finishes with \s . The filler must exclude '>' symbol!
             // attr + @"\s*=\s*""" + attrValue + @"""   -> here matches the attr = "value" (with as many spaces as it wants)
-            // .*>                                      -> some fillers (other attributes or so)
-            // (.*\b                                    -> fillers and beginning of a word
+            // [^>]*>                                   -> some fillers (other attributes or so) and follows the closing '>' of the opening part of the element
+            // Til now I have read <TAGNAME attr1="value1" and so on..> 
+            // ([^<]*\b                                 -> fillers and beginning of a word and must exclude the starting symbol of the closing part of the element '<'
             // + attrValue + @"\b                       -> the attribute value as separate word in the "text" field of the leaf element
-            string regexPattern = @"\s*(\w+)\b.*\s" + attr + @"\s*=\s*""" + attrValue + @""".*>(.*\b" + attrValue + @"\b.*)<";
-
+            // [^<]*)<                                  -> some filler to the end of the text part of the element and after that it's the closing part of the element '</TAGNAME>' (starting with '<' symbol)
+            string regexPattern = @"<\s*(\w+)\b[^>]*\s" + attr + @"\s*=\s*""" + attrValue + @"""[^>]*>([^<]*\b" + attrValue + @"\b[^<]*)<";
+            
             foreach (Match m in Regex.Matches(text, regexPattern, options))
             {
                 Console.WriteLine("Leaf [{0}] has text [{1}]", m.Groups[1].Value, m.Groups[2].Value); // Prints the leaf name and the 'text' field of element
