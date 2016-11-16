7608f61dc64abd2766354923d96531c035ab320e
Anton94 <antonvdudov@gmail.com>
2016-11-16 12:10:17 +0200

    Modified it to work with only two parents and to be more declarative

--- Genetic matrix generator/Genetic matrix generator/Matrix.cs
+++ Genetic matrix generator/Genetic matrix generator/Matrix.cs
@@ -7,53 +7,29 @@ namespace Genetic_matrix_generator
 {
     public class Matrix
     {
-        public double[,] matrix;
-        public int size;
+        public double[][] matrix;
+        public int n;
         public int life;
-        public static int[] lifeRange;
+        public static int lifeMin, lifeMax;
 
-        public static void setLifeRange(int minValue, int maxValue) // range == [min, max]
+        public Matrix(int n, double val = 0.0)
         {
-            if (minValue > maxValue)
-                throw new Exception("Invalid range for the life of the matrixes!");
-
-            lifeRange = new int[2];
-            lifeRange[0] = minValue;
-            lifeRange[1] = maxValue;
+            this.n = n;
+            matrix = new double[n][].
+                        Select(row => row = new double[n].
+                            Select(element => element = val).
+                            ToArray()).
+                        ToArray();
+            life = Program.rnd.Next(lifeMin, lifeMax + 1);
         }
 
-        public Matrix(int n)
+        public Matrix setAvarage(Matrix a, Matrix b) // expect at least two element array!
         {
-            createMatrix(n);
-        }
-        public Matrix(int n, double val, int life)
-        {
-            createMatrix(n);
-            setDefaultValues(val);
-            this.life = life;
-        }
-
-        public Matrix setAvarage(List<Matrix> m) // expect at least two element array!
-        {
-            // TODO: check matrix sizes if needed
-            //    throw new Exception("Error in set avarage of matrix!");
-            if (m.Count <= 0)
-                return this;
-
-            for (int i = 0; i < size; ++i)
-                for (int j = 0; j < size; ++j)
-                    this[i, j] = sum(m, i, j) / m.Count; // Very bad, cache misses a lot but for now is good...
+            foreach(int i in Enumerable.Range(0, matrix[0].Count()))
+                foreach(int j in Enumerable.Range(0, matrix[0].Count())) // TODO: if .Count is O(n) then cache it...
+                    matrix[i][j] = (a.matrix[i][j] + b.matrix[i][j]) / 2.0; // I need to know which indexes are at this place in the matrix
 
             return this;
-        }
-
-        private double sum(List<Matrix> matrixes, int i, int j)
-        {
-            double sum = 0.0;
-
-            matrixes.ForEach(m => sum += m[i, j]);
-
-            return sum;
         }
 
         public Matrix DecLife()
@@ -62,75 +38,21 @@ namespace Genetic_matrix_generator
             return this;
         }
 
-        public Matrix setLife()
-        {
-            life = MyRand.Next(lifeRange); // Returns random number in range[ lifeRange[0] , lifeRange[1] ]
-
-            return this;
-        }
-
-        public double this[int i, int j]
-        {
-            get
-            {
-                return matrix[i, j];
-            }
-            set
-            {
-                matrix[i, j] = value;
-            }
-        }
-
         public override string ToString()
         {
             string res = "";
 
-            for (int i = 0; i < size; ++i)
-            {
-                for (int j = 0; j < size; ++j)
+            foreach (int i in Enumerable.Range(0, matrix[0].Count()))
+            { 
+                foreach (int j in Enumerable.Range(0, matrix[0].Count()))
                 {
-                    res += matrix[i, j].ToString() + " ";
+                    res += matrix[i][j].ToString() + " ";
                 }
                 res += "\n";
             }
             res += "Life = " + life.ToString() + " \n";
 
             return res;
-        }
-
-        // Sets all cells to @val value...
-        public Matrix setDefaultValues(double val)
-        {
-            for (int i = 0; i < size; ++i)
-            {
-                for (int j = 0; j < size; ++j)
-                {
-                    matrix[i, j] = val;
-                }
-            }
-
-            return this;
-        }
-
-        // Multiplies the value in the cell by it's i * j indexes
-        public Matrix multiplyByIndexes()
-        {
-            for (int i = 0; i < size; ++i)
-            {
-                for (int j = 0; j < size; ++j)
-                {
-                    matrix[i, j] *= (i + 1) * (j + 1);
-                }
-            }
-
-            return this;
-        }
-
-        private void createMatrix(int n)
-        {
-            size = n;
-            matrix = new double[size, size];
-            setLife();
         }
     }
 }
--- Genetic matrix generator/Genetic matrix generator/Program.cs
+++ Genetic matrix generator/Genetic matrix generator/Program.cs
@@ -6,28 +6,9 @@ namespace Genetic_matrix_generator
 { 
     public class Program
     {
-        // Generates @count random indexes 
-        // and after that returns a list with @count "pointers" to the matrixes on thows random generated indexes of the given @list.
-        private static List<Matrix> listGetRandomElements(List<Matrix> list, int count, bool printDebugInfo)
-        {
-            List<int> rands = MyRand.NextRandsInRange(0, list.Count, count).ToList();
-            
-            // Only for debugging purposes...
-            if (printDebugInfo)
-            {
-                Console.Write("Chosen indexes: ");
-                foreach (int i in rands)
-                    Console.Write(i + " ");
+        // Random seed.
+        public static Random rnd;
 
-                Console.Write("\nAnd matrixes:\n");
-                foreach (int i in rands)
-                    Console.Write(list[i] + "\n");
-            }
-            return rands.Select(i => list[i]).ToList();
-
-            // Without debugging info:
-            // return MyRand.NextRandsInRange(0, list.Count, count).Select(i => list[i]).ToList();
-        }
 
         // Adds a matrix to the list and returns the list.
         private static List<Matrix> listAdd(Matrix m, List<Matrix> list)
@@ -42,22 +23,27 @@ namespace Genetic_matrix_generator
             Console.WriteLine("Matrixes:");
             list.ForEach(m => Console.Write(m));
         }
-        
-        // The iterator...
-        public static IEnumerable<Matrix> generateMatrix(List<Matrix> inputList, int numOfParents = 2, int COUNT = int.MaxValue, bool printDebugInfo = false) // int.MaxValue - a bad style of infinity... but good enough for now...
-        {
-            int n = inputList[0].size; // Just to be more clear
 
-            List<Matrix> list = new List<Matrix>(inputList);
+        // This function is only for debugging purposes, I can put list[rnd.Next(0, list.Count)
+        private static Matrix getRandFromList(List<Matrix> list, bool printDebugInfo)
+        {
+            int idx = rnd.Next(0, list.Count);
+            if (printDebugInfo) Console.Write("Chosen index: " + idx + " and matrix:\n" + list[idx]);
+
+            return list[idx]; 
+        }
+
+        // The iterator...
+        public static IEnumerable<Matrix> generateMatrix(Matrix a, Matrix b, bool printDebugInfo = false)
+        {
+            List<Matrix> list = new List<Matrix> { a, b };
             
-            for (int i = 0; i < COUNT; ++i)
+            while (true)
             {
-                if (printDebugInfo)
-                    listPrint(list);
-                yield return listAdd
-                            (
+                if (printDebugInfo) listPrint(list);
+                yield return listAdd(
                             // First - find the new element depending on the current states of the matrixes in the list.
-                            new Matrix(n).setAvarage(listGetRandomElements(list, numOfParents, printDebugInfo)),
+                            new Matrix(a.n).setAvarage(getRandFromList(list, printDebugInfo), getRandFromList(list, printDebugInfo)),
                             // after that apply the decrementing of matrixes lifes("map") and get those with lifes <= 0 out of the list ("filter").
                             list = list.Select(m => m.DecLife()).ToList().Where<Matrix>(m => m.life > 0).ToList()
                             ). // And add the new generated matrix to the list - "listAdd".
@@ -66,25 +52,20 @@ namespace Genetic_matrix_generator
         }
 
         // Generates @COUNT matrixes with sizes @SIZEx@SIZE 
-        // and values - chooses @numParents(number of parents) random matrixes of already generated (and alive) once and makes average.
+        // and values - chooses two random matrixes of already generated (and alive) once and makes average.
         // printDebugInfo- prints (or not) info for the chosen random matrixes and current matrixes in the list.
-        public static void test(int SIZE, int COUNT, int numParents, bool printDebugInfo)
+        public static void test(int SIZE, int COUNT, bool printDebugInfo)
         {
-            Matrix.setLifeRange(numParents, numParents + 3); // Setting life range of the matrixes
-            MyRand.setSeed(); // And the seed for the random numbers...
+            Matrix.lifeMin = 2;
+            Matrix.lifeMax = 5;
+            rnd = new Random(0);
 
-            // Just to generate some starting matrixes(depending on the @numParents, the lifes and starting matrixes must be grater than @numParents, otherwise the population may die...)
-            List<Matrix> inputList = new List<Matrix>();
-            for (int i = 0; i < numParents; ++i)
-            {
-            //    inputList.Add(new Matrix(SIZE, i, numParents + (i % 3)).multiplyByIndexes()); // To test with "different" cell values.
-                inputList.Add(new Matrix(SIZE, i, numParents + (i % 3)));
-            }
-
-            foreach (Matrix m in generateMatrix(inputList, numParents, COUNT, printDebugInfo))
-            {
-                Console.WriteLine("New generated:\n" + m);
-            }
+            generateMatrix(new Matrix(SIZE, 1.0), new Matrix(SIZE, 5.0), printDebugInfo).
+                            Take(COUNT).ToList().
+                            ForEach(m => Console.WriteLine("New generated:\n" + m)); 
+            // The debugging info will be before the new generated and the life will not be with the generated value
+            // because first generates the @COUNT matrixes, and after that prints them, and the generator modifies the generated once
+            // decrements their lifes...
         }
 
         public static void Main(string[] args)
@@ -94,7 +75,7 @@ namespace Genetic_matrix_generator
             // number of matrixes to generate,
             // number of matrixes, which is taken the avarage of to generate the new one,
             // with (true) or without(false) debuging info for the list of matrixes and choosen random matrixes.
-            test(5, 4, 2, true);            
+            test(5, 4, true);            
         }
     }
 }
